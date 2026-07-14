3809d2e00066018c388ecb49d3a64f2553ea5586
afrilaknaf <afrilaknaf036@gmail.com>
2026-07-14 15:26:06 +0530

    frist commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+    return (
+        <div className="container">
+            <h1>Jenkins Frontend Demo</h1>
+
+            <p>
+                React application built using Jenkins Pipeline
+            </p>
+
+            <button>Deploy Successfully</button>
+        </div>
+    );
+}
+
+export default App;
